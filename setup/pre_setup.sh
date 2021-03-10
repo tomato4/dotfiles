74da3b5ec04b952700ddea250db669b6889759b0
Tomáš Glázr <tom.gla45@gmail.com>
2021-03-10 17:44:42 +0100

    fixed req. sudo need

--- setup/pre_setup.sh
+++ setup/pre_setup.sh
@@ -6,7 +6,10 @@ source ./messages.sh
 # This script checks all requirements for setup
 
 message_info "Checking requirements for setup scripts."
-sudo pacman -Sq --needed --noconfirm rofi > /dev/null
+if ! [[ $(pacman -Qe | grep "rofi ") ]]
+then
+   sudo pacman -Sq --needed --noconfirm rofi > /dev/null
+fi
 
 for file in $DOTFILES_SETUP/scripts/*
 do
