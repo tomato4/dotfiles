ec403d9079dc217fac32d487b447c33d72536729
Tomáš Glázr <tom.gla45@gmail.com>
2021-03-28 00:56:09 +0100

    added i3 configs automatically chossen by env variable

--- setup/scripts/link_i3_config.sh
+++ setup/scripts/link_i3_config.sh
@@ -1,14 +1,14 @@
 message_info "Compiling and linking i3 config file."
-echo -n "Choose which i3 configuration (variables pack) you want:"
-for file in $DOTFILES_I3_CONFIGS/*
-do
-   echo -n " ${file##*/}"
-done
-echo ""
-
 cat $DOTFILES_I3/header > $DOTFILES_I3/config
 
-SEL_CONFIG=$(for config in $DOTFILES_I3_CONFIGS/*; do echo "$(basename $config)"; done | rofi -dmenu -format 's' 2> /dev/null)
+#SEL_CONFIG=$(for config in $DOTFILES_I3_CONFIGS/*; do echo "$(basename $config)"; done | rofi -dmenu -format 's' 2> /dev/null)
+if ! [[ -f $DOTFILES_I3_CONFIGS/$DOTFILES_ENV ]]
+then
+   message_error "I3 config for '$DOTFILES_ENV' not found!"
+   return
+fi
+
+SEL_CONFIG=$DOTFILES_ENV
 
 cat $DOTFILES_I3_CONFIGS/$SEL_CONFIG >> $DOTFILES_I3/config
 cat $DOTFILES_I3/base >> $DOTFILES_I3/config
