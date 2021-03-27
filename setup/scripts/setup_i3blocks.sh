ccdbd55b6681a86c6a63527d1fd39aa2329f90a3
Tomáš Glázr <tom.gla45@gmail.com>
2021-03-27 22:17:32 +0100

    i3blocks depending on env

--- setup/scripts/setup_i3blocks.sh
+++ setup/scripts/setup_i3blocks.sh
@@ -5,6 +5,15 @@ for file in $DOTFILES_I3BLOCK/scripts/*
 do
    chmod +x $file
 done
+
+CONFIG_FILE="config_$DOTFILES_ENV"
+if ! [[ -f $DOTFILES_I3BLOCK/$CONFIG_FILE ]]
+then
+   CONFIG_FILE="config_default"
+fi
+rm $DOTFILES_I3BLOCK/config 2> /dev/null
+ln -s $DOTFILES_I3BLOCK/$CONFIG_FILE $DOTFILES_I3BLOCK/config
+
 rm -rf $CONFIG/i3blocks
 ln -s $DOTFILES/i3blocks $CONFIG/i3blocks
 message_done "Linked i3blocks config folder."
