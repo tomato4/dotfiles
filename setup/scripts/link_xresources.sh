e37656bb01e5522ff432c500061018239a19f5d7
Tomáš Glázr <tom.gla45@gmail.com>
2021-03-28 01:30:23 +0100

    few changes

---
+++ setup/scripts/link_xresources.sh
@@ -0,0 +1,11 @@
+message_info "Linking Xresources."
+if ! [[ -f $DOTFILES_CONFIGS/Xresources/.Xresources_$DOTFILES_ENV ]]
+then
+   message_error "Xresources for '$DOTFILES_ENV' not found. This request will be ignored."
+   return
+fi
+
+rm $SETUP_HOME/.Xresources 2> /dev/null
+ln -s $DOTFILES_CONFIGS/.Xresources/.Xresources_$DOTFILES_ENV $SETUP_HOME/.Xresources
+
+message_done "Linked Xresources."
