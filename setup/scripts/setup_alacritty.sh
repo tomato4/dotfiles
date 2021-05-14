3a7df266534f12869d0b84af0e48eafc62620826
Tomáš Glázr <tom.gla45@gmail.com>
2021-05-14 23:32:13 +0200

    setup script fix

--- setup/scripts/setup_alacritty.sh
+++ setup/scripts/setup_alacritty.sh
@@ -1,5 +1,6 @@
-message_info "Installing alacritty."
-sudo pacman -S alacritty > /dev/null
+message_info "Installing alacritty and Hack Nerd Font."
+sudo pacman -S --needed --noconfirm alacritty > /dev/null
+sudo pamac install nerd-fonts-hack --no-confirm > /dev/null
 message_info "Linking config file."
 rm -rf $CONFIG/alacritty
 ln -s $DOTFILES/alacritty $CONFIG/alacritty
