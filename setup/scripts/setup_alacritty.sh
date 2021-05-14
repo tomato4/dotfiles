13b7862f65d8d2b138ad4e2d912c38c08ced69eb
Tomáš Glázr <tom.gla45@gmail.com>
2021-05-14 23:42:30 +0200

    fix 2

--- setup/scripts/setup_alacritty.sh
+++ setup/scripts/setup_alacritty.sh
@@ -1,6 +1,7 @@
 message_info "Installing alacritty and Hack Nerd Font."
 sudo pacman -S --needed --noconfirm alacritty > /dev/null
 sudo pamac install nerd-fonts-hack --no-confirm > /dev/null
+sudo pamac install python36 --no-confirm > /dev/null # for crypto script
 message_info "Linking config file."
 rm -rf $CONFIG/alacritty
 ln -s $DOTFILES/alacritty $CONFIG/alacritty
