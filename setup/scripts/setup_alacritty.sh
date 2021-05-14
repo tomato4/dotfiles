81797e9ee2ba347ae1d43f05f50fadd5a73ddc67
Tomáš Glázr <tom.gla45@gmail.com>
2021-05-15 00:08:10 +0200

    mess fix - wrong files

--- setup/scripts/setup_alacritty.sh
+++ setup/scripts/setup_alacritty.sh
@@ -1,9 +1,6 @@
 message_info "Installing alacritty and Hack Nerd Font."
 sudo pacman -S --needed --noconfirm alacritty > /dev/null
 sudo pamac install nerd-fonts-hack --no-confirm > /dev/null
-sudo pamac install python36 --no-confirm > /dev/null # for crypto script
-curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6 # install pip for python 3.6
-pip3.6 install requests # crypto
 message_info "Linking config file."
 rm -rf $CONFIG/alacritty
 ln -s $DOTFILES/alacritty $CONFIG/alacritty
--- setup/scripts/setup_polybar.sh
+++ setup/scripts/setup_polybar.sh
@@ -4,8 +4,13 @@ sudo pacman -S --needed --noconfirm ttf-polybar-icons > /dev/null
 sudo pamac install --no-confirm polybar-spotify-listener > /dev/null
 sudo pamac install --no-confirm ttf-weather-icons > /dev/null
 sudo pamac install --no-confirm ttf-font-awesome > /dev/null
+sudo cp $DOTFILES/polybar/modules_binary/polybar-cryptocurrency/coins.otf /usr/share/fonts/Coins.otf
 systemctl --user enable spotify-listener
 systemctl --user start spotify-listener
+
+sudo pamac install python36 --no-confirm > /dev/null # for crypto script
+curl https://bootstrap.pypa.io/get-pip.py | sudo -H python3.6 # install pip for python 3.6
+pip3.6 install requests # crypto
 
 message_info "Linking config file."
 rm -rf $CONFIG/polybar
