a0c83851bdc93f93f12a91821284ab7c527ad9b6
Tomáš Glázr <tom.gla45@gmail.com>
2021-05-22 00:27:38 +0200

    added some fish abbr and functions

---
+++ fish/functions/mkcd.fish
@@ -0,0 +1,4 @@
+function mkcd --description 'Create dir and cd to it.'
+    mkdir $argv[1]
+    cd $argv[1]
+end
---
+++ fish/functions/mon2cam.fish
@@ -0,0 +1,3 @@
+function mon2cam
+    deno run --unstable -A -r -q ~/Programs/Mon2Cam/src/mod.ts
+end
