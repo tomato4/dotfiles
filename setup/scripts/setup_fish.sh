b9c6d8a48d606df09e06a123f2a4f23b85578daa
Tomáš Glázr <tom.gla45@gmail.com>
2021-05-26 19:30:36 +0200

    fish setup checks for OMF

--- setup/scripts/setup_fish.sh
+++ setup/scripts/setup_fish.sh
@@ -3,7 +3,12 @@ install_pacman fish fastfetch
 rm -rf $CONFIG/fish
 ln -s $DOTFILES/fish $CONFIG/fish
 
-message_info "Installing Oh My Fish."
-curl -L https://get.oh-my.fish | fish
+if ! [ -f "$DOTFILES/fish/conf.d/omf.fish" ]
+then
+    message_info "Installing Oh My Fish."
+    curl -L https://get.oh-my.fish | fish
+else
+    message_done "OMF already installed..."
+fi
 
 message_done "Fish setup completed."
