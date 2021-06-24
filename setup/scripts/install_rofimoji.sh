e44e86e140a7dfe772cd19ed344104fb7a927952
Tomáš Glázr <tom.gla45@gmail.com>
2021-06-24 20:08:17 +0200

    rofimoji emoji font fix

--- setup/scripts/install_rofimoji.sh
+++ setup/scripts/install_rofimoji.sh
@@ -1,3 +1,3 @@
 message_info "Installing rofimoji."
-install_pacman rofimoji emoji-fonts rofi xdotool xsel
+install_pacman rofimoji noto-fonts-emoji rofi xdotool xsel
 message_done "Rofimoji installed."
