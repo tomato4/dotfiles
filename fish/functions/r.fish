f65321ba08f8a6fdccc42421f09b7a24df43cc0b
Tomáš Glázr <tom.gla45@gmail.com>
2021-09-29 14:09:23 +0200

    fish update

--- fish/functions/r.fish
+++ fish/functions/r.fish
@@ -1,6 +1,6 @@
 function r
     mkdir -p ~/.config/ranger/sessions/
-    find $HOME/.config/ranger/sessions/ -mindepth 1 -mtime +2 -exec rm {} \;
+    find $HOME/.config/ranger/sessions/ -mindepth 1 -mtime +2 -exec rm {} \; 2> /dev/null
     set SID (tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
     ranger --choosedir=$HOME/.config/ranger/sessions/$SID
     cd (cat ~/.config/ranger/sessions/$SID)
