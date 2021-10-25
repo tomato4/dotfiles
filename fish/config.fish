6c7ac611a7f021d44f582fe5cc04941544e0d12e
Tomáš Glázr <tom.gla45@gmail.com>
2021-10-26 00:09:45 +0200

    clean up

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias mon2cam="deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts"
