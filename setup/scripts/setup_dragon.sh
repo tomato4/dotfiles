6e36a552fa8eb9c7124c9ff27a3bb3a8caf74b55
Tomáš Glázr <tom.gla45@gmail.com>
2022-03-08 02:11:12 +0100

    some chagnes

--- setup/scripts/setup_dragon.sh
+++ setup/scripts/setup_dragon.sh
@@ -1,6 +1,3 @@
 message_info "Installing dragon (Drag&Drop)"
-install_pacman clang
-cd $DOTFILES_PROGRAMS/dragon/
-make > /dev/null
-make install > /dev/null
+install_yay dragon-drag-and-drop
 message_done "Installed dragon."
