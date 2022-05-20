15d9b23a24c167be55b7716fdab097efbb821305
Tomáš Glázr <tom.gla45@gmail.com>
2022-05-20 18:09:41 +0200

    remove zsh login config

--- setup/scripts/setup_zsh.sh
+++ setup/scripts/setup_zsh.sh
@@ -4,8 +4,6 @@ install_yay zsh zsh-syntax-highlighting zsh-autosuggestions
 
 rm -f $SETUP_HOME/.zshrc
 ln -s $DOTFILES/zsh/.zshrc $SETUP_HOME/.zshrc
-rm -f $SETUP_HOME/.zshlogin
-ln -s $DOTFILES/zsh/.zshlogin $SETUP_HOME/.zshlogin
 rm -f $SETUP_HOME/.p10k.zsh
 ln -s $DOTFILES/zsh/.p10k.zsh $SETUP_HOME/.p10k.zsh
 
