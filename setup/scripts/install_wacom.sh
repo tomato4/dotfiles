7fc7c6d1877b725a1bf1e7aea482f3563ca8e208
Tomáš Glázr <tom.gla45@gmail.com>
2022-06-08 17:24:56 +0200

    dracula submodule, some other changes

---
+++ setup/scripts/install_wacom.sh
@@ -0,0 +1,5 @@
+message_info "Installing xf86-input-wacom"
+
+install_yay xf86-input-wacom
+
+message_done "Installation done"
--- setup/scripts/setup_xournal.sh
+++ setup/scripts/setup_xournal.sh
@@ -2,4 +2,7 @@ install_pacman xournalpp
 
 rm -rf $SETUP_HOME/.xournalpp
 ln -s $DOTFILES/xournal $SETUP_HOME/.xournalpp
+
+rm -rf $CONFIG/xournalpp
+ln -s $DOTFILES/xournal $CONFIG/xournalpp
 message_done "Xournal setup done."
