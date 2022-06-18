dc8711ee41e2bb81aa0792b843e7066f9f913d12
Tomáš Glázr <tom.gla45@gmail.com>
2022-06-18 21:01:02 +0200

    xkb keyboard update

--- setup/scripts/link_xkb.sh
+++ setup/scripts/link_xkb.sh
@@ -1,6 +1,14 @@
 message_info "Applying my keyboard settings."
+
 sudo rm -f /usr/share/X11/xkb/symbols/cz
 sudo ln -s $DOTFILES_XKB/cz_modified /usr/share/X11/xkb/symbols/cz
-setxkbmap cz -model pc105
+
+sudo rm -f /usr/share/X11/xkb/rules/base.xml
+sudo ln -s $DOTFILES_XKB/base.xml /usr/share/X11/xkb/rules/base.xml
+
+sudo rm -f /etc/X11/xorg.conf.d/00-keyboard.conf
+sudo ln -s $DOTFILES_XKB/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
+
+setxkbmap -model pc104 -layout cz -variant my-layout
 message_done "Linked my keyboard settings."
 
