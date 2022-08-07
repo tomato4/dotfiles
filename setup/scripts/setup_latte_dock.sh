7431e81cf75a85a217a2833d518abdafd8b8e312
Tomáš Glázr <tom.gla45@gmail.com>
2022-08-07 21:54:56 +0200

    latte dock init

---
+++ setup/scripts/setup_latte_dock.sh
@@ -0,0 +1,12 @@
+message_info "Installing latte-dock"
+
+install_yay "latte-dock"
+
+message_info "Linking config files"
+
+rm -rf $CONFIG/latte/
+ln -s $DOTFILES/latte-dock/latte $CONFIG/latte
+rm -f $CONFIG/lattedockrc
+ln -s $DOTFILES/latte-dock/lattedockrc $CONFIG/lattedockrc
+
+message_done "Latte dock installed and set up"
