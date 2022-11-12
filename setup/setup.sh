9bfc4e371998a8d6aff3a816b0d8c641b3ca54f8
Tomáš Glázr <tom.gla45@gmail.com>
2022-11-12 17:28:00 +0100

    script refactor

--- setup/setup.sh
+++ setup/setup.sh
@@ -15,19 +15,19 @@ then
 fi
 
 # set env variable
-export DOTFILES_ENV=$(cat $DOTFILES_ENV_FILE)
+export DOTFILES_ENV=$(cat "$DOTFILES_ENV_FILE")
 
 # check setups for this env
 source ./env_checker.sh
 
 # if script is run with arguments - scripts
 if [ $# -ne 0 ]; then
-    OUTPUT=$(sed 's/ /\n/g' <<< "$@")
+    OUTPUT=${@// /\\n}
     PRINT=$@
 else
     # Open rofi menu with all commands available
     message_info "Opening menu for commands. Multiple items can be selected by holding shift."
-    OUTPUT=$(for file in ./scripts/*; do echo "$(basename $file)"; done | rofi -dmenu -multi-select -format 's\n' 2> /dev/null | sed 's/\\n//g')
+    OUTPUT=$(for file in ./scripts/*; do basename "$file"; done | rofi -dmenu -multi-select -format 's\n' 2> /dev/null | sed 's/\\n//g')
     PRINT=$(echo "$OUTPUT" | sed -z 's/\n/, /g')
 fi
 
@@ -35,10 +35,10 @@ message_info "Selected commands: ${PRINT::-2}"
 while IFS= read -r line
 do
    # skip empty line
-   if ! [ $line ]; then continue; fi
+   if ! [ "$line" ]; then continue; fi
 
    # run each selected script
-   ./scripts/$line
+   ./scripts/"$line"
 
 done <<< "$OUTPUT"
 
