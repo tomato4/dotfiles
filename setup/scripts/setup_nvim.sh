152744d98396dd2f6a74011f681f918a8bde7b2c
Tomáš Glázr <tom.gla45@gmail.com>
2023-05-23 00:45:58 +0200

    add nvim plug to nvim setup

--- setup/scripts/setup_nvim.sh
+++ setup/scripts/setup_nvim.sh
@@ -4,5 +4,6 @@ rm -rf $CONFIG/nvim
 ln -s $DOTFILES/nvim $CONFIG/nvim
 message_done "Linked neovim config folder."
 
+source $DOTFILES_SETUP/scripts/setup_nvim_plug.sh
 source $DOTFILES_SETUP/scripts/setup_askpass.sh
 source $DOTFILES_SETUP/scripts/install_nodejs.sh
