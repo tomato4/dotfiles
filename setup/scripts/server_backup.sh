09385afdba47b3afb5ad26ad5f43d4fb3429bf0b
Tomáš Glázr <tom.gla45@gmail.com>
2023-06-23 21:39:09 +0200

    servr backup script

--- setup/scripts/server_backup.sh
+++ setup/scripts/server_backup.sh
@@ -1,7 +1,9 @@
-EXTERNAL_DIR="/run/media/tomato/drive"
-EXTERNAL_DEV="/dev/sdc1"
-EXTERNAL_PATH_BACKUP="/Backup/Server/$(date +'%Y_%m_%d').tar.gz"
-SERVER_DIR="/mount/md0"
+SERVER_DIR="/mnt/md0"
+SERVER_USER="root@10.0.1.39"
+BACKUP_USER="tomato@10.0.1.16"
+BACKUP_DEV="/dev/sdc1"
+BACKUP_DIR="/run/media/tomato/drive"
+BACKUP_REPO_PATH="/Backup/server::$(date +'%Y_%m_%d')"
 
 message_info "Input directories without trailing slash!"
 
@@ -11,29 +13,55 @@ then
   SERVER_DIR=$input
 fi
 
-input=$(input "Backup device ($EXTERNAL_DEV)")
+input=$(input "Server user ($SERVER_USER)")
 if [ -n "$input" ]
 then
-  EXTERNAL_DEV=$input
+  SERVER_USER=$input
 fi
 
-input=$(input "Backup device mount directory ($EXTERNAL_DIR)")
+input=$(input "Backup user ($BACKUP_USER)")
 if [ -n "$input" ]
 then
-  EXTERNAL_DIR=$input
+  BACKUP_USER=$input
 fi
 
-input=$(input "Backup location ($EXTERNAL_PATH_BACKUP)")
+while :
+do
+  input=$(input "Backup device [? for list] ($BACKUP_DEV)")
+  if [ "$input" == "?" ]
+  then
+    echo
+    lsblk
+    echo
+    continue
+  elif [ -n "$input" ]
+  then
+    BACKUP_DEV=$input
+  fi
+  break
+done
+
+input=$(input "Backup device mount directory ($BACKUP_DIR)")
 if [ -n "$input" ]
 then
-  EXTERNAL_PATH_BACKUP=$input
+  BACKUP_DIR=$input
+fi
+
+input=$(input "Backup repository ($BACKUP_REPO_PATH)")
+if [ -n "$input" ]
+then
+  BACKUP_REPO_PATH=$input
 fi
 
 message_info "Configuration:"
+echo
 echo "Server directory: $SERVER_DIR"
-echo "Backup device: $EXTERNAL_DEV"
-echo "Backup device mount directory: $EXTERNAL_DIR"
-echo "Backup location: $EXTERNAL_PATH_BACKUP"
+echo "Server user: $SERVER_USER"
+echo "Backup user: $BACKUP_USER"
+echo "Backup device: $BACKUP_DEV"
+echo "Backup device mount directory: $BACKUP_DIR"
+echo "Backup repository: $BACKUP_REPO_PATH"
+echo
 
 if ! confirm "Proceed with this configuration?"
 then
@@ -41,14 +69,14 @@ then
   exit 0
 fi
 
-if grep -qs "$EXTERNAL_DEV" /proc/mounts
+if grep -qs "$BACKUP_DEV" /proc/mounts
 then
-    if grep -qs "$EXTERNAL_DEV $EXTERNAL_DIR " /proc/mounts
+    if grep -qs "$BACKUP_DEV $BACKUP_DIR " /proc/mounts
     then
         message_warn "External drive is already mounted on correct location. Skipping mount..."
     else
-        EXTERNAL_DIR=$(findmnt -no TARGET $EXTERNAL_DEV)
-        message_warn "External drive is already connected but on different location. ($EXTERNAL_DIR)"
+        BACKUP_DIR=$(findmnt -no TARGET "$BACKUP_DEV")
+        message_warn "External drive is already connected but on different location. ($BACKUP_DIR)"
         if ! confirm "Should I proceed?"
         then
             message_warn "Aborted..."
@@ -56,19 +84,23 @@ then
         fi
     fi
 else
-    sudo mkdir -p "$EXTERNAL_DIR"
-    sudo mount "$EXTERNAL_DEV" "$EXTERNAL_DIR"
-    message_done "Successfully mounted '$EXTERNAL_DEV' to '$EXTERNAL_DIR'."
+    sudo mkdir -p "$BACKUP_DIR"
+    sudo mount "$BACKUP_DEV" "$BACKUP_DIR"
+    message_done "Successfully mounted '$BACKUP_DEV' to '$BACKUP_DIR'."
 fi
 
-sudo tar -cvzf "$EXTERNAL_DIR$EXTERNAL_PATH_BACKUP" "$SERVER_DIR"
+# shellcheck disable=SC2029
+ssh -t "$SERVER_USER" "
+BORG_RELOCATED_REPO_ACCESS_IS_OK=true
+borg create --progress '$BACKUP_USER:$BACKUP_DIR$BACKUP_REPO_PATH' '$SERVER_DIR'
+" < /dev/tty
 
 message_done "Backup finished."
 
 if confirm "Unmount and shutdown backup drive?"
 then
-  sudo umount "$EXTERNAL_DIR"
-  sudo hdparm -y "$EXTERNAL_DEV"
+  sudo umount "$BACKUP_DIR"
+  sudo hdparm -y "$BACKUP_DEV"
 fi
 
 message_done "Successfully unmounted drive."
