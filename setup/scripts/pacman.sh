6815c4bdef95491909cf0b964af91dfe64124a28
Tomáš Glázr <tom.gla45@gmail.com>
2023-07-31 18:22:46 +0200

    pacman mirrorlist

--- setup/scripts/pacman.sh
+++ setup/scripts/pacman.sh
@@ -1,5 +1,8 @@
 message_info "Linking pacman conf file."
 sudo_link "$DOTFILES/pacman/pacman.conf" /etc/pacman.conf
 
+message_info "Linking pacman mirrorlist."
+sudo_link "$DOTFILES/pacman/mirrorlist" /etc/pacman.d/mirrorlist
+
 message_info "Disabling pamac mirrorlist timer (to prevent mirrorlist autoupdates)"
 sudo systemctl mask pamac-mirrorlist.timer
